ace004054d3602de6275db9104c1b282ecb2b763
Karamysh <dezenu@ya.ru>
2023-09-26 12:54:08 +0300

    add stylish file and diff tree creator. Step 6

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/029396f1068d07a15256/maintainability)](https://codeclimate.com/github/Karamysh/php-project-48/maintainability)
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/029396f1068d07a15256/test_coverage)](https://codeclimate.com/github/Karamysh/php-project-48/test_coverage)
+
+### Asciinema
+
+[![asciicast](https://asciinema.org/a/610232.svg)](https://asciinema.org/a/610232)
+
+[![asciicast](https://asciinema.org/a/610234.svg)](https://asciinema.org/a/610234)
--- src/Differ.php
+++ src/Differ.php
@@ -4,6 +4,8 @@ namespace Differ\Differ;
 
 use function Differ\Parsers\parseJsonFile;
 use function Differ\Parsers\parseYamlFile;
+use function Differ\DiffFinder\createDiffTree;
+use function Differ\Stylish\makeOutputTree;
 
 function getParsedData($pathToFile)
 {
@@ -11,7 +13,7 @@ function getParsedData($pathToFile)
     switch ($format) {
         case 'json':
             return parseJsonFile($pathToFile);
-        case 'yaml':
+        case 'yaml' || 'yml':
             return parseYamlFile($pathToFile);
         default:
             return false;
@@ -30,27 +32,8 @@ function genDiff($pathToFile1, $pathToFile2)
 {
     $data1 = getParsedData($pathToFile1);
     $data2 = getParsedData($pathToFile2);
-    $mergedDataKeys = array_keys(array_merge($data1, $data2));
-    sort($mergedDataKeys);
 
-    return array_reduce(
-        $mergedDataKeys,
-        function ($diffAccum, $key) use ($data1, $data2) {
-            $element1 = convertToStrIfBool($data1[$key] ?? '');
-            $element2 = convertToStrIfBool($data2[$key] ?? '');
-
-            if (array_key_exists($key, $data1) && array_key_exists($key, $data2)) {
-                if ($data1[$key] === $data2[$key]) {
-                    return "{$diffAccum}    {$key}: {$element1}\n";
-                } else {
-                    return "{$diffAccum}  - {$key}: {$element1}\n  + {$key}: {$element2}\n";
-                }
-            } elseif (array_key_exists($key, $data1)) {
-                return "{$diffAccum}  - {$key}: {$element1}\n";
-            } else {
-                return "{$diffAccum}  + {$key}: {$element2}\n";
-            }
-        },
-        "\n{\n"
-    ) . "}";
+    $tree = createDiffTree($data1, $data2);
+    $stylisedTree = makeOutputTree($tree);
+    return $stylisedTree;
 }
